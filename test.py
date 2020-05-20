dbb9a8e873574a3cd7b4c686179c0e683b2bd29b
MarkDWilliams <markwilliams2755@gmail.com>
2020-05-20 16:58:46 -0400

    initial testing implementation

---
+++ test.py
@@ -0,0 +1,9 @@
+from contextlib import closing
+import requests
+import json
+url ='http://transltr.io:7071/validate_querygraph'
+testJson = json.loads(open('imatinibQuery.json').read())
+with closing(requests.post(url, json=testJson, stream=False)) as response:
+    status_code = response.status_code
+    assert status_code == 200
+
